6fde64257f9224ea621ca9b1a47affa67ee5e583
anggitanatarina <anggitanatarina@gmail.com>
2025-02-16 10:37:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const burger = document.querySelector('.burger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    burger.addEventListener('click', function() {
-        navLinks.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-    });
-});
+// script.js
+function toggleMenu() {
+    var menu = document.querySelector('.nav-links');
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
