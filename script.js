7260991efedd30d0128cf2fa2b19e05ad4b303c0
anggitanatarina <anggitanatarina@gmail.com>
2025-02-16 10:45:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function toggleMenu() {
         menu.style.display = "flex";
     }
 }
+
+function createStars() {
+    const starsContainer = document.querySelector('.stars');
+    for (let i = 0; i < 20; i++) { // Membatasi jumlah bintang
+        let star = document.createElement('div');
+        star.classList.add('star');
+        star.style.left = Math.random() * 100 + 'vw';
+        star.style.top = Math.random() * 100 + 'vh';
+        star.style.animationDuration = (Math.random() * 5 + 3) + 's';
+        starsContainer.appendChild(star);
+    }
+}
